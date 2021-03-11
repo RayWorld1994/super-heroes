96da62b605ed061bc6fefb6852f94401d1428448
Erick <erykede@gmail.com>
2021-03-11 04:00:24 -0600

    🏷️ Adding type and array for comic format

---
+++ src/app/modules/comics/util/arrayFormComic.ts
@@ -0,0 +1,12 @@
+import { FormatComic } from '../../core/interfaces/comic/formatComic.type';
+
+export const AFormatComic: Array<FormatComic> = [
+  'comic',
+  'digest',
+  'digital comic',
+  'graphic nivel',
+  'hardcover',
+  'infinite comic',
+  'magazine',
+  'trade paparback',
+];
---
+++ src/app/modules/core/interfaces/comic/formatComic.type.ts
@@ -0,0 +1,9 @@
+export type FormatComic =
+  | 'comic'
+  | 'magazine'
+  | 'trade paparback'
+  | 'hardcover'
+  | 'digest'
+  | 'graphic nivel'
+  | 'digital comic'
+  | 'infinite comic';
