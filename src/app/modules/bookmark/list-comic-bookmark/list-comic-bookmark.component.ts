f3103eb19ae557c26b734ae987ca6c6784d029a8
Erick <erykede@gmail.com>
2021-03-11 23:55:23 -0600

    ✨ Adding bookmark for comic

--- src/app/modules/bookmark/list-comic-bookmark/list-comic-bookmark.component.ts
+++ src/app/modules/bookmark/list-comic-bookmark/list-comic-bookmark.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Comic } from '../../core/interfaces/comic/comic.interface';
+import * as comicSelectors from 'src/app/modules/core/store/selectors/comic.selector';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list-comic-bookmark',
   templateUrl: './list-comic-bookmark.component.html',
-  styleUrls: ['./list-comic-bookmark.component.scss']
+  styleUrls: ['./list-comic-bookmark.component.scss'],
 })
 export class ListComicBookmarkComponent implements OnInit {
+  comics: Observable<Comic[]> = this.store.select(
+    comicSelectors.getComicsBookmarks
+  );
 
-  constructor() { }
+  constructor(private store: Store) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
