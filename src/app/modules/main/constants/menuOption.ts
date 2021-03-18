814a5e59d72aee24919b34fdabf84df4562a6bed
Erick <erykede@gmail.com>
2021-03-18 03:36:36 -0600

    🚧 Commenting out unused route

--- src/app/modules/main/constants/menuOption.ts
+++ src/app/modules/main/constants/menuOption.ts
@@ -14,8 +14,8 @@ export const menuOption: Option[] = [
   { option: 'Bookmarks', route: '/bookmarks', icon: faStar },
   { option: 'Characters', route: '/characters', icon: faMask },
   { option: 'Comics', route: '/comics', icon: faBook },
-  { option: 'Series', route: '/series', icon: faFilm },
   { option: 'Stories', route: '/stories', icon: faHistory },
-  { option: 'Events', route: '/events', icon: faCalendar },
-  { option: 'Creators', route: '/creators', icon: faPencilRuler },
+  // { option: 'Series', route: '/series', icon: faFilm },
+  // { option: 'Events', route: '/events', icon: faCalendar },
+  // { option: 'Creators', route: '/creators', icon: faPencilRuler },
 ];
