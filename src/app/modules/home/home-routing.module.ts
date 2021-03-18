94e0fd521a03f8308a479bd60628f6ba8e3f8f2a
Erick <erykede@gmail.com>
2021-03-18 01:11:25 -0600

    ✨ declaring component

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,10 +1,11 @@
+import { HomeLayoutComponent } from './home-layout/home-layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: HomeLayoutComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
